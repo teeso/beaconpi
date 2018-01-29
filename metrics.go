f1acba3fde88e35406c9bdc14299a20ab0df39a1
Bradley Kennedy <bk@co60.ca>
2018-01-29 17:18:03 -0500

    Debugging

--- metrics.go
+++ metrics.go
@@ -261,7 +261,7 @@ func trilat(results []result, edgeloc [][]float64, dbm int) []locationResults {
 	// thereafter, this should ensure the edgeloc slice is also in
 	// the correct order.
 	sort.Slice(results, func(i, j int) bool {
-		return results[i].Bracket.Before(results[j].Bracket) && results[i].Edge < results[j].Edge
+		return results[i].Bracket.Before(results[j].Bracket) || results[i].Edge < results[j].Edge
 	})
 	log.Printf("Results, sorted: %#v", results)
 	var tempresults []result
